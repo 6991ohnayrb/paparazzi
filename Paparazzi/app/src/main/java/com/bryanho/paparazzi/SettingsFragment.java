91dbb8b91808d3c1ba7db091a5be9a704fda05ac
Bryan Ho <6991ohnayrb@gmail.com>
2018-01-30 00:36:31 -0800

    Populate first and last name

--- Paparazzi/app/src/main/java/com/bryanho/paparazzi/SettingsFragment.java
+++ Paparazzi/app/src/main/java/com/bryanho/paparazzi/SettingsFragment.java
@@ -6,8 +6,17 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.Profile;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SettingsFragment extends Fragment {
+
+    @BindView(R.id.first_name) TextView firstNameTextView;
+    @BindView(R.id.last_name) TextView lastNameTextView;
 
     public SettingsFragment() {
     }
@@ -20,6 +29,14 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_new_game, container, false);
+        final View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        ButterKnife.bind(this, view);
+        populateFields();
+        return view;
+    }
+
+    private void populateFields() {
+        firstNameTextView.setText(Profile.getCurrentProfile().getFirstName());
+        lastNameTextView.setText(Profile.getCurrentProfile().getLastName());
     }
 }
