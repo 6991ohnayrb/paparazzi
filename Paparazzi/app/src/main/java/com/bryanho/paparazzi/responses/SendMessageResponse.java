35c50bfbb6cca46bea14333882a938c926cb7e8f
Bryan Ho <6991ohnayrb@gmail.com>
2018-02-14 20:12:11 -0800

    Add serialized variable in response

--- Paparazzi/app/src/main/java/com/bryanho/paparazzi/responses/SendMessageResponse.java
+++ Paparazzi/app/src/main/java/com/bryanho/paparazzi/responses/SendMessageResponse.java
@@ -1,4 +1,11 @@
 package com.bryanho.paparazzi.responses;
 
+import com.google.gson.annotations.SerializedName;
+
+import lombok.Data;
+
+@Data
 public class SendMessageResponse {
+    @SerializedName("messagestatus") private String messageStatus;
+    @SerializedName("timestamp") private int timestamp;
 }
