c59f60f0630ed5d77b27c9527123bb9f95917553
Kelvin Wong <kelvinwong623@gmail.com>
2018-02-27 19:01:21 -0800

    added logging that logs hash key to logcat

--- Paparazzi/app/src/main/java/com/bryanho/paparazzi/activities/LoginActivity.java
+++ Paparazzi/app/src/main/java/com/bryanho/paparazzi/activities/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.bryanho.paparazzi.activities;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.bryanho.paparazzi.R;
@@ -18,6 +23,9 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
@@ -33,6 +41,25 @@ public class LoginActivity extends PaparazziActivity {
         setContentView(R.layout.activity_login);
 
         initializeStethoscope(getApplicationContext());
+
+        PackageInfo info;
+        try {
+            info = getPackageManager().getPackageInfo("com.bryanho.paparazzi", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                //String something = new String(Base64.encodeBytes(md.digest()));
+                Log.e("hash key", something);
+            }
+        } catch (PackageManager.NameNotFoundException e1) {
+            Log.e("name not found", e1.toString());
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("no such an algorithm", e.toString());
+        } catch (Exception e) {
+            Log.e("exception", e.toString());
+        }
 
         callbackManager = CallbackManager.Factory.create();
 
